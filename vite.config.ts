7a10520d7f6c50096b301f271966b51f357c01ce
Hauryk <hauryk.andrey@gmail.com>
2025-10-21 16:36:36 +0300

    fix vite config for stories

--- vite.config.ts
+++ vite.config.ts
@@ -1,45 +1,46 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import svgLoader from 'vite-svg-loader'
-import dts from 'vite-plugin-dts'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import svgLoader from 'vite-svg-loader';
+import dts from 'vite-plugin-dts';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     svgLoader({
-      defaultImport: 'component'
+      defaultImport: 'component',
     }),
     dts({
       rollupTypes: true,
-      tsconfigPath: './tsconfig.app.json'
-    })
+      tsconfigPath: './tsconfig.app.json',
+      exclude: ['src/stories', '**/*.stories.ts', '**/*.stories.tsx'],
+    }),
   ],
   resolve: {
     alias: {
-      '@': '/src'
-    }
+      '@': '/src',
+    },
   },
   build: {
     lib: {
       entry: 'src/index.ts',
       name: 'NestroUiKit',
       fileName: (format) => `nestro-ui-kit.${format}.js`,
-      formats: ['es', 'umd']
+      formats: ['es', 'umd'],
     },
     rollupOptions: {
       external: ['vue'],
       output: {
         globals: {
-          vue: 'Vue'
+          vue: 'Vue',
         },
         assetFileNames: (assetInfo) => {
           if (assetInfo.name && assetInfo.name.endsWith('.css')) {
             return 'nestro-ui-kit.css';
           }
           return assetInfo.name || 'assets/[name].[ext]';
-        }
-      }
-    }
-  }
-})
+        },
+      },
+    },
+  },
+});
