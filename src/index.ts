3978762dd4c79902995ff7cac30eed5323649d58
Арсен Меркин <a-merkin@MacBook-Air-Arsen.local>
2026-01-29 20:06:19 +0700

    tokens in components

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ import './styles/colors.css';
 
 export { default as Button } from './components/Button/Button.vue';
 export { default as Icon } from './components/Icon/Icon.vue';
-export { default as Table } from './components/Table/Table.vue';
 export { default as Dropdown } from './components/Dropdown/Dropdown.vue';
 export { default as Checkbox } from './components/Checkbox/Checkbox.vue';
 export { default as Switch } from './components/Switch/Switch.vue';
